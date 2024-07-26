07fcec6bae3a878e7feff4525dc922a0fc37a033
Aliaksandr Liakh <aaa@epam.com>
2024-07-26 12:53:54 +0300

    Add

--- src/main/java/prep/PrepTask3.java
+++ src/main/java/prep/PrepTask3.java
@@ -13,6 +13,7 @@ import com.commercetools.api.models.common.AddressBuilder;
 import com.commercetools.api.models.customer.Customer;
 import com.commercetools.api.models.customer.CustomerChangeAddressActionBuilder;
 import com.commercetools.api.models.customer.CustomerDraftBuilder;
+import com.commercetools.api.models.customer.CustomerPagedQueryResponse;
 import com.commercetools.api.models.customer.CustomerUpdateBuilder;
 import com.commercetools.api.models.product.ProductProjection;
 import com.commercetools.api.models.product.ProductProjectionPagedSearchResponse;
@@ -41,236 +42,24 @@ public class PrepTask3 {
                 ApiPrefixHelper.API_POC_CLIENT_PREFIX.getPrefix()
             );
 
-        Project project = apiRoot
-            .withProjectKey("cool-store")
-            .get()
-            .execute()
-            .get()
-            .getBody();
+//      HTTP API queries and Query Predicates
+        String countryCode = "DE";
 
-        logger.info("countries: {}", project.getCountries());
-        logger.info("currencies: {}", project.getCurrencies());
-        logger.info("languages: {}", project.getLanguages());
-        logger.info("trial until: {}", project.getTrialUntil());
-
-        String customerEmail = "john_doe@email.com";
-        String customerPassword = "john_doe_password";
-        String customerKey = "john_doe";
-        String customerFirstName = "John";
-        String customerLastName = "Doe";
-        String customerCountry = "DE";
-
-        // Update a Customer's phone number:
-        //  Create a function that takes a Customer key, version, and updates the phone number.
-        //  Handle version mismatch.
-
-        Customer customer1 = apiRoot
+//        Write a function that retrieves all Customers from a specific country.
+        CustomerPagedQueryResponse response = apiRoot
             .customers()
-            .post(CustomerDraftBuilder.of()
-                .email(customerEmail)
-                .password(customerPassword)
-                .firstName(customerFirstName)
-                .lastName(customerLastName)
-                .key(customerKey)
-                .addresses(
-                    AddressBuilder.of()
-                        .key(customerKey + "-" + customerCountry)
-                        .country(customerCountry)
-                        .build()
-                )
-                .defaultShippingAddress(0)
-                .build())
-            .execute()
             .get()
-            .getBody()
-            .getCustomer();
-
-        Address address1 = customer1.getAddresses().get(0);
-        logger.info("old phone: {}", address1.getPhone());
-
-        Customer customer2 = apiRoot
-            .customers()
-            .withKey(customer1.getKey())
-            .post(CustomerUpdateBuilder.of()
-                .version(customer1.getVersion())
-                .actions(
-                    CustomerChangeAddressActionBuilder.of()
-                        .addressKey(address1.getKey())
-                        .address(
-                            AddressBuilder.of(address1)
-                                .phone(String.valueOf(Math.abs(new Random().nextInt())))
-                                .build()
-                        )
-                        .build()
-                )
-                .build()
-            )
-            .execute()
-            .get()
-            .getBody();
-
-        Address address2 = customer2.getAddresses().get(0);
-        logger.info("new phone: {}", address2.getPhone());
-
-        apiRoot
-            .customers()
-            .withKey(customer2.getKey())
-            .delete()
-            .withVersion(customer2.getVersion())
-            .execute()
-            .get()
-            .getBody();
-
-        // Check if a specific Product Variant (identified by an sku) is in stock for a given Store.
-        //  Fetch the Product Variant using Product Search or Product Projection Search.
-        //  Determine if there is at least one Inventory entry for the Store.
-        //  Console log a user-friendly message indicating availability.
-
-        ProductProjectionPagedSearchResponse response = apiRoot
-            .productProjections()
-            .search()
-            .get()
-            .withFilterQuery("variants.sku:\"RWG-09\"")
+            .withWhere("addresses(country=\"" + countryCode + "\")")
             .executeBlocking()
             .getBody();
 
-        Optional<ProductProjection> productProjection = response.getResults().stream().findFirst();
-        if (productProjection.isPresent()) {
-            ProductVariant variant = productProjection.get().getMasterVariant();
-            logger.info("variant: {}", variant);
-            logger.info("is on stock: {}", variant.getAvailability().getIsOnStock());
-        } else {
-            logger.info("no variants found");
-        }
+        response
+            .getResults()
+            .forEach(customer ->
+                logger.info("customer: {}", customer.getFirstName() + " " + customer.getLastName())
+            );
 
-        // Build a set of functions to manage a basic Shopping Cart.
-        //  Create a new Cart for an anonymous user.
-        //  Add a Product (which has available inventory) to the Cart.
-        //  Increase the quantity of the existing Line Item in the Cart (check that you have enough inventory for this item to complete the action).
-        //  Make multiple changes to the Cart in a single request. This should include creating a Cart, adding an email address, Line Item, Country, and Locale.
 
-        Cart cart1 = apiRoot
-            .carts()
-            .post(
-                CartDraftBuilder.of()
-                    .currency("EUR")
-                    .deleteDaysAfterLastModification(90L)
-                    .anonymousId("anonymous" + System.nanoTime())
-                    .country("DE")
-                    .build()
-            )
-            .execute()
-            .get()
-            .getBody()
-            .get();
-        logger.info("anonymous cart created: {}", cart1);
-
-        Cart cart2 = apiRoot
-            .carts()
-            .withId(cart1.getId())
-            .get()
-            .execute()
-            .thenComposeAsync(cartApiHttpResponse -> {
-                    Cart cart = cartApiHttpResponse.getBody();
-                    return apiRoot.carts()
-                        .withId(cart.getId())
-                        .post(
-                            CartUpdateBuilder.of()
-                                .version(cart.getVersion())
-                                .actions(
-                                    Stream.of("RWG-09")
-                                        .map(sku -> CartAddLineItemActionBuilder.of()
-                                            .sku(sku)
-                                            .build()
-                                        )
-                                        .collect(Collectors.toList())
-                                )
-                                .build()
-                        )
-                        .execute();
-                }
-            )
-            .get().getBody();
-
-        LineItem lineItem2 = cart2.getLineItems().stream().findFirst().orElseThrow();
-        logger.info("product added to cart: {}",
-            "product key: " + lineItem2.getProductKey() + ", quantity " + lineItem2.getQuantity()
-        );
-
-        Cart cart3 = apiRoot.carts()
-            .withId(cart2.getId())
-            .post(
-                CartUpdateBuilder.of()
-                    .version(cart2.getVersion())
-                    .actions(
-                        CartChangeLineItemQuantityActionBuilder.of()
-                            .lineItemId(lineItem2.getId())
-                            .quantity(2L)
-                            .build()
-                    )
-                    .build()
-            )
-            .execute().get().getBody();
-
-        LineItem lineItem3 = cart3.getLineItems().stream().findFirst().orElseThrow();
-        logger.info("product increased to cart: {}",
-            "product key: " + lineItem3.getProductKey() + ", quantity " + lineItem3.getQuantity()
-        );
-
-        Cart cart4 = apiRoot
-            .carts()
-            .post(
-                CartDraftBuilder.of()
-                    .currency("EUR")
-                    .deleteDaysAfterLastModification(90L)
-                    .anonymousId("anonymous" + System.nanoTime())
-                    .country("DE")
-                    .build()
-            )
-            .execute()
-            .thenComposeAsync(cartApiHttpResponse -> {
-                    Cart cart = cartApiHttpResponse.getBody();
-                    return apiRoot.carts()
-                        .withId(cart.getId())
-                        .post(
-                            CartUpdateBuilder.of()
-                                .version(cart.getVersion())
-                                .actions(
-                                    Stream.of(
-                                            CartUpdateActionBuilder.of()
-                                                .setCustomerEmailBuilder().email(Math.abs(new Random().nextInt()) + "@email.com")
-                                                .build(),
-                                            CartAddLineItemActionBuilder.of()
-                                                .sku("RWG-09")
-                                                .build(),
-                                            CartUpdateActionBuilder.of()
-                                                .setCountryBuilder().country("AU")
-                                                .build(),
-                                            CartUpdateActionBuilder.of()
-                                                .setLocaleBuilder().locale("en-AU")
-                                                .build()
-                                        )
-                                        .collect(Collectors.toList())
-                                )
-                                .build()
-                        )
-                        .execute();
-                }
-            )
-            .get()
-            .getBody()
-            .get();
-
-//        LineItem lineItem4 = cart4.getLineItems().stream().findFirst().orElseThrow();
-        logger.info("cart created: {}",
-            " customer email: " + cart4.getCustomerEmail() +
-//                ", line item: product key: " + lineItem4.getProductKey() + ", quantity " + lineItem4.getQuantity() +
-                ", country: " + cart4.getCountry() +
-                ", locale: " + cart4.getLocale()
-        );
-
-//      HTTP API queries and Query Predicates
-//        Write a function that retrieves all Customers from a specific country.
 //        Modify the previous function to retrieve Customers from a specific city within a Country, ensuring the search is case-insensitive. (Hint: explore Query Predicates in the documentation).
 //        Make a request to get data for a Product Listing Page.
 //          Facets displayed should be: Price and Category
