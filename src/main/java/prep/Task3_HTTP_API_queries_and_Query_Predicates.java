5e318a798e97e73cdf74d9720576363f46f3b202
Aliaksandr Liakh <aaa@epam.com>
2024-07-26 17:35:00 +0300

    Add

--- src/main/java/prep/Task3_HTTP_API_queries_and_Query_Predicates.java
+++ src/main/java/prep/Task3_HTTP_API_queries_and_Query_Predicates.java
@@ -1,46 +1,25 @@
 package prep;
 
 import com.commercetools.api.client.ProjectApiRoot;
-import com.commercetools.api.models.cart.Cart;
-import com.commercetools.api.models.cart.CartAddLineItemActionBuilder;
-import com.commercetools.api.models.cart.CartChangeLineItemQuantityActionBuilder;
-import com.commercetools.api.models.cart.CartDraftBuilder;
-import com.commercetools.api.models.cart.CartUpdateActionBuilder;
-import com.commercetools.api.models.cart.CartUpdateBuilder;
-import com.commercetools.api.models.cart.LineItem;
 import com.commercetools.api.models.category.CategoryReference;
-import com.commercetools.api.models.common.Address;
-import com.commercetools.api.models.common.AddressBuilder;
 import com.commercetools.api.models.common.BaseAddress;
-import com.commercetools.api.models.customer.Customer;
-import com.commercetools.api.models.customer.CustomerChangeAddressActionBuilder;
-import com.commercetools.api.models.customer.CustomerDraftBuilder;
-import com.commercetools.api.models.customer.CustomerPagedQueryResponse;
-import com.commercetools.api.models.customer.CustomerUpdateBuilder;
-import com.commercetools.api.models.product.ProductPagedQueryResponse;
-import com.commercetools.api.models.product.ProductProjection;
 import com.commercetools.api.models.product.ProductProjectionPagedQueryResponse;
 import com.commercetools.api.models.product.ProductProjectionPagedSearchResponse;
-import com.commercetools.api.models.product.ProductVariant;
-import com.commercetools.api.models.project.Project;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import prep.impl.ApiPrefixHelper;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static prep.impl.ClientService.createApiClient;
 
-public class PrepTask3 {
+public class Task3_HTTP_API_queries_and_Query_Predicates {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        Logger logger = LoggerFactory.getLogger(PrepTask1b_CREATE_CARTS.class.getName());
+        final Logger logger = LoggerFactory.getLogger(PrepTask1b_CREATE_CARTS.class.getName());
 
         final ProjectApiRoot apiRoot =
             createApiClient(
